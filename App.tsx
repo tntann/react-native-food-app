af79c7736dd7967a563bb52770323714c89c1d11
Tan Tran <tan.tran@sapotacorp.vn>
2024-11-03 22:52:03 +0700

    moving between screens

--- App.tsx
+++ App.tsx
@@ -1,22 +1,49 @@
-import { Text, View } from "react-native";
+import { Button, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { useNavigation } from '@react-navigation/native';
 
 export default function App() {
   const Stack = createNativeStackNavigator();
 
   function HomeScreen() {
+    const navigation:any = useNavigation();
     return (
       <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
         <Text>Home Screen</Text>
+        <View style={{paddingVertical: 10}}>
+        <Button
+          title="Go to Detail"
+          onPress={() => navigation.navigate("Details")}
+        />
+        </View>
+        <View style={{paddingVertical: 10}}>
+        <Button
+          title="Go to user id = 1"
+          onPress={() => navigation.navigate("Details")}
+        />
+        </View>
+        <View style={{paddingVertical: 10}}>
+        <Button
+          title="Go to user id = 2"
+          onPress={() => navigation.navigate("Details")}
+        />
+        </View>
       </View>
     );
   }
 
   function DetailsScreen() {
+    const navigation:any = useNavigation();
     return (
       <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
         <Text>Details Screen</Text>
+        <Text>user id ???</Text>
+        <Button
+          title="Go Back Home"
+          // onPress={() => navigation.navigate("Home")}
+          onPress={() => navigation.goBack()}
+        />
       </View>
     );
   }
