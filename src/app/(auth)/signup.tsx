66e376e69aec8342492a8b0b99f90799d938d3a3
Tan Tran <nhattan8421@gmail.com>
2024-11-14 12:09:11 +0700

    call API with react-native

--- src/app/(auth)/signup.tsx
+++ src/app/(auth)/signup.tsx
@@ -2,8 +2,9 @@ import ShareButton from "@/components/button/share.button";
 import SocialButton from "@/components/button/social.button";
 import ShareInput from "@/components/input/share.input";
 import { APP_COLOR } from "@/utils/constant";
+import axios from "axios";
 import { Link } from "expo-router";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
@@ -16,9 +17,24 @@ const styles = StyleSheet.create({
 });
 
 const SignUpPage = () => {
+  const URL_BACKEND = process.env.EXPO_PUBLIC_API_URL;
+  console.log(">>>>check url", URL_BACKEND);
+
   const [name, setName] = useState<string>("");
   const [email, setEmail] = useState<string>("");
   const [password, setPassword] = useState<string>("");
+
+  useEffect(() => {
+    const fetchAPI = async () => {
+      try {
+        const res = await axios.get(URL_BACKEND!);
+        console.log(">>>> check res: ", res.data);
+      } catch (error) {
+        console.log(">>>> check error: ", error);
+      }
+    };
+    fetchAPI();
+  }, []);
 
   return (
     <SafeAreaView style={{ flex: 1 }}>
