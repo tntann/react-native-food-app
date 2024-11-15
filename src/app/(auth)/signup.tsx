31a7a343d6fd8a97fa7367af957041f8b6c9cc0b
Tan Tran <nhattan8421@gmail.com>
2024-11-15 16:43:28 +0700

    display error message

--- src/app/(auth)/signup.tsx
+++ src/app/(auth)/signup.tsx
@@ -6,6 +6,7 @@ import { APP_COLOR } from "@/utils/constant";
 import { Link, router } from "expo-router";
 import { useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
+import Toast from "react-native-root-toast";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 const styles = StyleSheet.create({
@@ -27,7 +28,15 @@ const SignUpPage = () => {
       if (res.data) {
         router.navigate("/(auth)/verify");
       } else {
-        alert(res.message);
+        const message = Array.isArray(res.message)
+          ? res.message[0]
+          : res.message;
+        Toast.show(message, {
+          duration: Toast.durations.LONG,
+          textColor: "white",
+          backgroundColor: APP_COLOR.ORANGE,
+          opacity: 1,
+        });
       }
     } catch (error) {
       console.log(">>>> check error: ", error);
