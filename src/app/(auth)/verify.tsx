12aa64285115d3a2057a76ce37f9ee7eb42f9d4f
Tan Tran <nhattan8421@gmail.com>
2024-11-15 17:34:42 +0700

    design verify code screen

--- src/app/(auth)/verify.tsx
+++ src/app/(auth)/verify.tsx
@@ -1,9 +1,47 @@
-import { Text, View } from "react-native";
+import { APP_COLOR } from "@/utils/constant";
+import { StyleSheet, Text, View } from "react-native";
+import OTPTextView from "react-native-otp-textinput";
+
+const styles = StyleSheet.create({
+  container: {
+    paddingVertical: 30,
+    paddingHorizontal: 20,
+  },
+  heading: {
+    fontSize: 25,
+    fontWeight: "600",
+    marginVertical: 20,
+  },
+});
 
 const VerifyPage = () => {
   return (
-    <View>
-      <Text>VerifyPage</Text>
+    <View style={styles.container}>
+      <Text style={styles.heading}>Xác thực tài khoản</Text>
+      <Text style={{ marginVertical: 10 }}>
+        Vui lòng nhập mã xác nhận đã được gửi tới địa chỉ email
+      </Text>
+      <View style={{ marginVertical: 20 }}>
+        <OTPTextView
+          inputCount={6}
+          inputCellLength={1}
+          tintColor={APP_COLOR.ORANGE}
+          textInputStyle={{
+            width: 40,
+            height: 40,
+            borderWidth: 1,
+            borderBlockColor: APP_COLOR.GRAY,
+            borderBottomWidth: 1,
+            borderRadius: 5,
+            // @ts-ignore:next-line
+            color: APP_COLOR.ORANGE,
+          }}
+        />
+      </View>
+      <View style={{ flexDirection: "row", marginHorizontal: 10 }}>
+        <Text>Không nhận được mã xác nhận! </Text>
+        <Text style={{ color: APP_COLOR.ORANGE }}>Gửi lại</Text>
+      </View>
     </View>
   );
 };
--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,32 +1,36 @@
 import { Stack } from "expo-router";
+import { SafeAreaView } from "react-native-safe-area-context";
 import { RootSiblingParent } from "react-native-root-siblings";
 
 const RootLayout = () => {
   return (
     <RootSiblingParent>
-      <Stack
-        screenOptions={{
-          headerStyle: {
-            backgroundColor: "#f4511e",
-          },
-          headerTintColor: "#fff",
-          headerTitleStyle: {
-            fontWeight: "bold",
-          },
-        }}
-      >
-        <Stack.Screen name="index" options={{ headerShown: false }} />
-        <Stack.Screen name="(auth)/signup" options={{ headerShown: false }} />
-        <Stack.Screen name="(tabs)" options={{ headerTitle: "Trang chủ" }} />
-        <Stack.Screen
-          name="product/index"
-          options={{ headerTitle: "Sản Phẩm" }}
-        />
-        <Stack.Screen
-          name="(auth)/login"
-          options={{ headerTitle: "Đăng nhập" }}
-        />
-      </Stack>
+      <SafeAreaView style={{ flex: 1 }}>
+        <Stack
+          screenOptions={{
+            headerStyle: {
+              backgroundColor: "#f4511e",
+            },
+            headerTintColor: "#fff",
+            headerTitleStyle: {
+              fontWeight: "bold",
+            },
+          }}
+        >
+          <Stack.Screen name="index" options={{ headerShown: false }} />
+          <Stack.Screen name="(auth)/signup" options={{ headerShown: false }} />
+          <Stack.Screen name="(auth)/verify" options={{ headerShown: false }} />
+          <Stack.Screen name="(tabs)" options={{ headerTitle: "Trang chủ" }} />
+          <Stack.Screen
+            name="product/index"
+            options={{ headerTitle: "Sản Phẩm" }}
+          />
+          <Stack.Screen
+            name="(auth)/login"
+            options={{ headerTitle: "Đăng nhập" }}
+          />
+        </Stack>
+      </SafeAreaView>
     </RootSiblingParent>
   );
 };
