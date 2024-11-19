2d81542c54e2f897c4cc08a6ad7b33362f98992c
Tan Tran <nhattan8421@gmail.com>
2024-11-19 21:29:10 +0700

    done list category

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -8,16 +8,16 @@ const data = Array(10).fill(1);
 
 const HomeTab = () => {
   return (
-    <SafeAreaView style={styles.container}>
-      <CustomFlatList
-        data={data}
-        style={styles.list}
-        renderItem={() => <View style={styles.item} />}
-        HeaderComponent={<HeaderHome />}
-        StickyElementComponent={<SearchHome />}
-        TopListElementComponent={<TopListHome />}
-      />
-    </SafeAreaView>
+    // <SafeAreaView style={styles.container}>
+    <CustomFlatList
+      data={data}
+      style={styles.list}
+      renderItem={() => <View style={styles.item} />}
+      HeaderComponent={<HeaderHome />}
+      StickyElementComponent={<SearchHome />}
+      TopListElementComponent={<TopListHome />}
+    />
+    // </SafeAreaView>
   );
 };
 
